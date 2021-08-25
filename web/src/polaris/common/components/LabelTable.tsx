7c6a7dcf14c3b80493d979381e4fc40e3754df81
tingfuyeh <tingfuyeh@tencent.com>
2021-08-25 16:06:45 +0800

    fix: 修复js文件加载缓慢问题

--- web/src/polaris/common/components/LabelTable.tsx
+++ web/src/polaris/common/components/LabelTable.tsx
@@ -1,9 +1,9 @@
-import { Table, SearchBox } from "tea-component";
-import React from "react";
-import { scrollable, autotip } from "tea-component/lib/table/addons";
+import { Table, SearchBox, Text } from 'tea-component'
+import React from 'react'
+import { scrollable, autotip } from 'tea-component/lib/table/addons'
 
 interface Props {
-  labels: Record<string, { value: string } | string>;
+  labels: Record<string, { value: string } | string>
 }
 export default function (props: Props) {
   if (!props.labels) {
@@ -11,49 +11,49 @@ export default function (props: Props) {
       <Table
         records={[]}
         columns={[
-          { key: "key", header: "标签键" },
-          { key: "key", header: "标签值" },
+          { key: 'key', header: '标签键' },
+          { key: 'key', header: '标签值' },
         ]}
         addons={[
           scrollable({
-            maxHeight: "500px",
+            maxHeight: '500px',
           }),
         ]}
       ></Table>
-    );
+    )
   }
-  const [searchWord, setSearchWord] = React.useState("");
+  const [searchWord, setSearchWord] = React.useState('')
 
-  const { labels } = props;
+  const { labels } = props
   const records = Object.keys(labels || {})
     .map((item) => ({
       key: item,
       value: labels[item],
     }))
-    .filter((item) => item.key.indexOf(searchWord) >= 0);
+    .filter((item) => item.key.indexOf(searchWord) >= 0)
   return (
     <>
       <SearchBox
         value={searchWord}
         onChange={(value) => setSearchWord(value)}
-        style={{ marginBottom: "20px" }}
+        style={{ marginBottom: '20px' }}
       ></SearchBox>
       <Table
         bordered
         records={records}
         columns={[
-          { key: "key", header: "标签键" },
-          { key: "value", header: "标签值" },
+          { key: 'key', header: '标签键', render: (x) => <Text tooltip={x.value}>{x.value}</Text> },
+          { key: 'value', header: '标签值', render: (x) => <Text tooltip={x.value}>{x.value}</Text> },
         ]}
         addons={[
           scrollable({
-            maxHeight: "500px",
+            maxHeight: '500px',
           }),
           autotip({
-            emptyText: "暂无标签",
+            emptyText: '暂无标签',
           }),
         ]}
       ></Table>
     </>
-  );
+  )
 }
