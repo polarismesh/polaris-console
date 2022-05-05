a55960564c954bc4f08c94231a1a110f16219dbf
tingfuyeh <tingfuyeh@tencent.com>
2022-05-05 10:52:20 +0800

    feat: 变更菜单位置

--- web/src/menu.ts
+++ web/src/menu.ts
@@ -18,22 +18,6 @@ export const MenuConfig = {
       icon: ['/static/img/namespace.svg', '/static/img/namespace.svg'],
     },
   },
-  observability: {
-    isGroup: true,
-    title: '可观测性',
-    'route-monitor': {
-      title: '路由监控',
-      icon: ['/static/img/route-monitor.svg', '/static/img/route-monitor.svg'],
-    },
-    'circuitBreaker-monitor': {
-      title: '熔断监控',
-      icon: ['/static/img/circuit-monitor.svg', '/static/img/circuit-monitor.svg'],
-    },
-    'ratelimit-monitor': {
-      title: '限流监控',
-      icon: ['/static/img/limit-monitor.svg', '/static/img/limit-monitor.svg'],
-    },
-  },
   ...(buildConfig.configuration
     ? {
         configuration: {
@@ -50,6 +34,22 @@ export const MenuConfig = {
         },
       }
     : {}),
+  observability: {
+    isGroup: true,
+    title: '可观测性',
+    'route-monitor': {
+      title: '路由监控',
+      icon: ['/static/img/route-monitor.svg', '/static/img/route-monitor.svg'],
+    },
+    'circuitBreaker-monitor': {
+      title: '熔断监控',
+      icon: ['/static/img/circuit-monitor.svg', '/static/img/circuit-monitor.svg'],
+    },
+    'ratelimit-monitor': {
+      title: '限流监控',
+      icon: ['/static/img/limit-monitor.svg', '/static/img/limit-monitor.svg'],
+    },
+  },
   auth: {
     isGroup: true,
     title: '权限控制',
