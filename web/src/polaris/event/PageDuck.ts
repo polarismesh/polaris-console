a5a841225c0d1e6a06faee720d43895f4007b865
chuntaojun <liaochuntao@live.com>
2022-12-09 09:37:02 +0800

    refactor:调整表格显示

--- web/src/polaris/event/PageDuck.ts
+++ web/src/polaris/event/PageDuck.ts
@@ -115,7 +115,8 @@ export default class ServicePageDuck extends GridPageDuck {
       namespaceList: (state: State) => state.namespaceList,
       eventTypeMap: (state: State) => {
         return state.eventTypeList.reduce((prev, curr) => {
-          return (prev[curr.type] = curr.desc)
+          prev[curr.type] = curr.desc
+          return prev
         }, {} as any)
       },
     }
