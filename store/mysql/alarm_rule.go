7925ffa8f7504110aeace572f587082e72cfdd91
chuntaojun <liaochuntao@live.com>
2022-12-22 12:40:52 +0800

    log:添加错误SQL打印

--- store/mysql/alarm_rule.go
+++ store/mysql/alarm_rule.go
@@ -237,15 +237,15 @@ func (a *alarmRuleStore) GetAlarmRuleByName(name string) (*alarm.AlarmRule, erro
 func (a *alarmRuleStore) GetAlarmRules(query map[string]string, offset, limit uint32) (uint32, []*alarm.AlarmRule, error) {
 
 	countSql := `
-	SELECT COUNT(*) FROM alarm_rule WHERE 1=1 
+	SELECT COUNT(*) FROM alarm_rule WHERE 
 	`
 	querySql := genAlarmRuleAllFieldsSQL() +
 		`
-	WHERE 1=1
+	WHERE 
 	`
 
 	args := []interface{}{}
-	tmps := []string{}
+	tmps := []string{"1=1"}
 	for k, v := range query {
 		v, isWild := commonhttp.ParseWildName(v)
 		if isWild {
@@ -257,8 +257,8 @@ func (a *alarmRuleStore) GetAlarmRules(query map[string]string, offset, limit ui
 		}
 	}
 
-	countSql += " AND " + strings.Join(tmps, " AND ")
-	querySql += " AND " + strings.Join(tmps, " AND ")
+	countSql += " " + strings.Join(tmps, " AND ")
+	querySql += " " + strings.Join(tmps, " AND ")
 	querySql += " ORDER BY mtime  LIMIT ? , ? "
 
 	var total uint32
