00d75e183b795feb8b61bad5a5eabd8956db258f
tingfuyeh <tingfuyeh@tencent.com>
2023-03-10 15:07:17 +0800

    fix: 调整线条粗细

--- web/src/polaris/monitor/registryMonitor/MetricCard.tsx
+++ web/src/polaris/monitor/registryMonitor/MetricCard.tsx
@@ -60,6 +60,7 @@ export default function(props: Props) {
                   return values.map(metaData => ({ ...metaData, valueText: roundToN(metaData.value, 2).toString() }))
                 },
               }}
+              size={1}
               theme={{ color: ['#006EFF', '#0ABF5B', '#E54545', '#FF7200', '#6E829D'] }}
             ></BasicArea>
           )}
