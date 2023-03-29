502bcf7daf6bcffa261a61793d147de51ac40785
chuntaojun <liaochuntao@live.com>
2023-03-29 16:37:21 +0800

    feat:页面功能开关

--- handlers/proxy.go
+++ handlers/proxy.go
@@ -74,6 +74,8 @@ func ReverseProxyForLogin(polarisServer *bootstrap.PolarisServer, conf *bootstra
 				log.Error("[Proxy][Login] modify login request fail", zap.Error(err))
 				return
 			}
+			req.Header["Content-Length"] = []string{fmt.Sprint(len(body))}
+			req.ContentLength = int64(len(body))
 			req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 		}
 		modifyResp := func(resp *http.Response) error {
