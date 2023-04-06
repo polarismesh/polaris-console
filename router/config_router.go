cdef8b3ab8e94ac8b299b2e612d98f1db8daef3c
chuntaojun <liaochuntao@live.com>
2023-04-07 00:21:43 +0800

    fix:修复配置导入导出NotFound

--- router/config_router.go
+++ router/config_router.go
@@ -32,7 +32,6 @@ func ConfigRouter(r *gin.Engine, config *bootstrap.Config) {
 	configV1.GET("configfilegroups", handlers.ReverseProxyForServer(&config.PolarisServer, config))
 	configV1.DELETE("configfilegroups", handlers.ReverseProxyForServer(&config.PolarisServer, config))
 	configV1.PUT("configfilegroups", handlers.ReverseProxyForServer(&config.PolarisServer, config))
-
 	// 配置文件
 	configV1.POST("configfiles", handlers.ReverseProxyForServer(&config.PolarisServer, config))
 	configV1.GET("configfiles", handlers.ReverseProxyForServer(&config.PolarisServer, config))
@@ -41,14 +40,14 @@ func ConfigRouter(r *gin.Engine, config *bootstrap.Config) {
 	configV1.PUT("configfiles", handlers.ReverseProxyForServer(&config.PolarisServer, config))
 	configV1.DELETE("configfiles", handlers.ReverseProxyForServer(&config.PolarisServer, config))
 	configV1.DELETE("configfiles/batchdelete", handlers.ReverseProxyForServer(&config.PolarisServer, config))
-
 	// 配置文件发布
 	configV1.POST("configfiles/release", handlers.ReverseProxyForServer(&config.PolarisServer, config))
 	configV1.GET("configfiles/release", handlers.ReverseProxyForServer(&config.PolarisServer, config))
-
 	// 配置文件发布历史
 	configV1.GET("configfiles/releasehistory", handlers.ReverseProxyForServer(&config.PolarisServer, config))
-
 	//配置文件模板
 	configV1.GET("configfiletemplates", handlers.ReverseProxyForServer(&config.PolarisServer, config))
+	// 配置导入导出
+	configV1.POST("configfiles/export", handlers.ReverseProxyForServer(&config.PolarisServer, config))
+	configV1.POST("configfiles/import", handlers.ReverseProxyForServer(&config.PolarisServer, config))
 }
