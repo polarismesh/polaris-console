5ea32526d40c2146c1c0a3bdfae811c4c4c04347
chuntaojun <liaochuntao@live.com>
2023-10-25 16:00:45 +0800

    stash code

--- web/src/polaris/configuration/fileGroup/detail/file/constants.ts
+++ web/src/polaris/configuration/fileGroup/detail/file/constants.ts
@@ -1,9 +1,14 @@
 export enum FileStatus {
+  Normal = 'normal',
   Success = 'success',
   Fail = 'failure',
   Edited = 'to-be-released',
 }
 export const FileStatusMap = {
+  [FileStatus.Normal]: {
+    text: '发布成功',
+    theme: 'success',
+  },
   [FileStatus.Success]: {
     text: '发布成功',
     theme: 'success',
