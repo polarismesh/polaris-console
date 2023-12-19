58b8238a4ae77402e2e76ac628e83e2d887a769e
chuntaojun <liaochuntao@live.com>
2023-12-19 17:50:01 +0800

    stash code

--- build_docker.sh
+++ build_docker.sh
@@ -9,15 +9,19 @@ if [ $# != 1 ]; then
 fi
 
 docker_tag=$1
-docker_repository="polarismesh"
+docker_repository="${DOCKER_REPOSITORY}"
+if [[ "${docker_repository}" == "" ]]; then
+    docker_repository="polarismesh"
+fi
 
-echo "docker repository : polarismesh/polaris-console, tag : ${docker_tag}"
+echo "docker repository : ${docker_repository}/polaris-console, tag : ${docker_tag}"
 
 arch_list=( "amd64" "arm64" )
 platforms=""
 
 for arch in ${arch_list[@]}; do
     export GOARCH=${arch}
+    export GOOS="linux"
     bash build.sh ${docker_tag}
     if [ $? != 0 ]; then
       echo "build polaris-console failed"
@@ -28,7 +32,7 @@ for arch in ${arch_list[@]}; do
     platforms+="linux/${arch},"
 done
 
-platforms=${platforms::-1}
+platforms=${platforms%?}
 extra_tags=""
 
 pre_release=`echo ${docker_tag}|egrep "(alpha|beta|rc|[T|t]est)"|wc -l`
@@ -36,4 +40,4 @@ if [ ${pre_release} == 0 ]; then
   extra_tags="-t ${docker_repository}/polaris-console:latest"
 fi
 
-docker buildx build --network=host -t ${docker_repository}/polaris-console:${docker_tag} ${extra_tags} --platform ${platforms} --push ./
+docker buildx build -t ${docker_repository}/polaris-console:${docker_tag} ${extra_tags} --platform ${platforms} --push ./
