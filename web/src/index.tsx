048a8fc92968beabd8b068e0fc44082d8db43e40
tingfuyeh <tingfuyeh@tencent.com>
2024-03-12 10:26:17 +0800

    fix: 去除login页面

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -9,17 +9,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 import { Router, Switch, Route } from 'react-router-dom'
-import LoginPage from '@src/polaris/auth/login/Page'
-import LoginPageDuck from '@src/polaris/auth/login/PageDuck'
-import { connectWithDuck } from './polaris/common/helpers'
 
-const Login = connectWithDuck(LoginPage, LoginPageDuck as any)
 export default function render() {
   ReactDOM.unmountComponentAtNode(document.querySelector('#polaris-console'))
   ReactDOM.render(
     <Router history={history}>
       <Switch>
-        <Route exact path='/login' component={Login} />
         <Route exact path='*'>
           <App />
         </Route>
