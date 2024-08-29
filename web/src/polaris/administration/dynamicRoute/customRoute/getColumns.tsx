e7da9635db01d7f8ea277a8c7c86fa8bcf08b06b
chuntaojun <liaochuntao@live.com>
2024-08-29 17:50:19 +0800

    fix:完善鉴权的页面适配

--- web/src/polaris/administration/dynamicRoute/customRoute/getColumns.tsx
+++ web/src/polaris/administration/dynamicRoute/customRoute/getColumns.tsx
@@ -99,12 +99,14 @@ export default (
           fn: (dispatch?: Dispatch<any>, e?) => void
           disabled: boolean
         }[] = [
-          {
-            id: 'switchStatus',
-            text: x.enable ? '禁用' : '启用',
-            fn: dispatch => {
-              const swtichStatusAction = x.enable ? SwitchStatusAction.disable : SwitchStatusAction.start
-              dispatch(creators.switchStatus(x.id, x.name, swtichStatusAction))
+            {
+              id: 'switchStatus',
+              text: x.enable ? '禁用' : '启用',
+              fn: dispatch => {
+                const swtichStatusAction = x.enable ? SwitchStatusAction.disable : SwitchStatusAction.start
+                dispatch(creators.switchStatus(x.id, x.name, swtichStatusAction))
+              },
+              disabled: !x.editable,
             },
             disabled: !x.editable,
           },
