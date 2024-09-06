9b47bf53d81b5ea13bca5e7b906ce05f7800f8c2
andrew shan <45474304+andrewshan@users.noreply.github.com>
2024-09-06 16:27:43 +0800

    feat: 支持页面展示控制的环境变量自动注入 (#283)

--- tool/start.sh
+++ tool/start.sh
@@ -14,6 +14,11 @@ workdir=$(pwd)
 #------------------------------------------------------
 source tool/include
 
+# 页面展示的控制配置
+if [[ -f "${workdir}/function.json" ]] && [[ -z "${POLARIS_EXPORT_FUNCTION_FILE}" ]];then
+  export POLARIS_EXPORT_FUNCTION_FILE="${workdir}/function.json"
+fi
+
 pids=$(ps -ef | grep -w "$cmdline" | grep -v "grep" | awk '{print $2}')
 array=($pids)
 if [ "${#array[@]}" == "0" ]; then
